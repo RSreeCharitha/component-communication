cc3487b420317e7eb69c39f54ae67effe2b6e81d
rsreecharitha <rsreecharitha@gmail.com>
2024-07-28 12:24:43 +0530

    skeleton

---
+++ force-app/main/default/lwc/compChild/compChild.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class CompChild extends LightningElement {}
---
+++ force-app/main/default/lwc/compGrandp/compGrandp.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class CompGrandp extends LightningElement {}
